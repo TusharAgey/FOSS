534c03591c495f8557fe5a8c68f20c2e504c1696
Tushar Agey <agey.tushar3@gmail.com>
2017-01-23 12:02:00 +0530

    shell script to make you pause system use after every 1hr for 5minutes

---
+++ Assignment1/interval.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+notify-send "Hello sir! lets have some fun this session!"
+while [ 1 -eq 1 ]
+do
+	sleep 1h
+	notify-send "Sir, Its been an hour. Please take a break of 5 minutes!"
+done
+#This shell script makes you suspend using laptop for 5 minutes!
